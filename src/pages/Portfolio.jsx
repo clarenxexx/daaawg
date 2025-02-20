9873092f7d9b186c4461b321ab52c3c56f0015db
clarenxexx <earlclarence2000@gmail.com>
2025-02-20 22:53:19 +0800

    Added a hero UI

---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../App.css'
+
+const Portfolio = () => {
+  return (
+    <>
+      <main>
+        <section class="hero">
+            <div class="hero-content">
+                <h2>Welcome to my Portfolio</h2>
+                <h2>Hi.</h2>
+                <h1>I'm Earl</h1>
+            </div>
+        </section>
+    </main>
+    </>
+  )
+}
+
+export default Portfolio
